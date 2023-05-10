8d475d450212d08dd266b4b7a519bd5e90668c8f
tr1ppyb0y <asatishobhit9@gmail.com>
2023-05-10 13:55:25 +0530

    Added database pipeline.

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -7,6 +7,7 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import re
+from mysql import connector
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
@@ -20,12 +21,9 @@ class BookscraperPipeline:
         
         # Converting ratings into integers.
         ratings = {
-            'zero': 0,
-            'one': 1,
-            'two': 2,
-            'three': 3,
-            'four': 4,
-            'five': 5
+            'zero': 0, 'one': 1,
+            'two': 2, 'three': 3,
+            'four': 4, 'five': 5
         }
         adapter['ratings'] = ratings[adapter['ratings']]
 
@@ -45,3 +43,64 @@ class BookscraperPipeline:
         adapter['number_of_reviews'] = int(adapter.get('number_of_reviews', 0))
 
         return item
+
+class SaveToMySQLPipeline:
+    
+    def __init__(self) -> None:
+        self.conn = connector.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='books'
+        )
+
+        # Create cursor.
+        self.cur = self.conn.cursor()
+
+        # Create Book table if not exists.
+        self.cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS books(
+                id int NOT NULL auto_increment,
+                url VARCHAR(255), title text,
+                upc VARCHAR(255), product_type VARCHAR(255),
+                price DECIMAL, price_excl_tax DECIMAL,
+                price_incl_tax DECIMAL, tax DECIMAL,
+                availability INTEGER,
+                number_of_reviews INTEGER,
+                category VARCHAR(255), ratings INTEGER,
+                description text, PRIMARY KEY (id)
+            )
+            """
+        )
+    
+    def process_item(self, item, spider):
+
+        # Define Insert statement.
+        self.cur.execute(
+            """
+            INSERT INTO books(
+                url, title, upc, product_type,
+                price_excl_tax, price_incl_tax,
+                tax, price, availability, number_of_reviews,
+                ratings, category, description
+            ) values(
+                %s, %s, %s, %s, %s, %s, 
+                %s, %s, %s, %s, %s, %s, %s
+            )
+            """, (
+                item['url'], item['title'], item['upc'],
+                item['product_type'], item['price_excl_tax'],
+                item['price_incl_tax'], item['tax'], item['price'],
+                item['availability'], item['number_of_reviews'],
+                item['ratings'], item['category'], item['description']
+            )
+        )
+        self.conn.commit()
+        return item
+    
+    def close_spider(self, spider):
+
+        # Close database connection.
+        self.cur.close()
+        self.conn.close()
